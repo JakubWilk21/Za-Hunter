2323cc883db6ad3984ac7f75ba5441f0f2892474
Jakub <Wilk>
2023-03-15 09:44:02 -0500

    fixed map location

--- Za Hunter/ContentView.swift
+++ Za Hunter/ContentView.swift
@@ -10,19 +10,29 @@ import MapKit
 struct ContentView: View {
     @StateObject var locationManager = LocationManager()
     @State private var userTrackingMode: MapUserTrackingMode = .follow
-    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 42.15704, longitude: -88.14812), span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 42.1539, longitude: -88.1362), span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
     @State private var places = [Place]()
     var body: some View {
-        Map(coordinateRegion: $region,
-            interactionModes: .all,
-            showsUserLocation: true,
-            userTrackingMode: $userTrackingMode,
-            annotationItems: places) { place in
-            MapAnnotation(coordinate: place.annotation.coordinate) {
-                NavigationLink(destination: LocationDetailsView(selectedMapItem: place.mapItem)) {
-                    Image("pizza")
-    }
-    func performSearch(item: String) {
+         NavigationView {
+             Map(
+                         coordinateRegion: $region,
+                         interactionModes: .all,
+                         showsUserLocation: true,
+                         userTrackingMode: $userTrackingMode,
+                         annotationItems: places) { place in
+                             MapAnnotation(coordinate: place.annotation.coordinate) {
+                                            Marker(mapItem: place.mapItem)
+                                        }
+                     }
+                     .onAppear(perform: {
+                         performSearch(item: "Pizza")
+                     })
+                     }
+             .navigationTitle("Za Hunter")
+             .navigationBarTitleDisplayMode(.inline)
+         }
+
+     func performSearch(item: String) {
         let searchRequest = MKLocalSearch.Request()
         searchRequest.naturalLanguageQuery = item
         searchRequest.region = region
@@ -34,16 +44,28 @@ struct ContentView: View {
                     annotation.coordinate = mapItem.placemark.coordinate
                     annotation.title = mapItem.name
                     places.append(Place(annotation: annotation, mapItem: mapItem))
-           
-       }
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
-        }
-        struct Place: Identifiable {
-            let id = UUID()
-            let annotation: MKPointAnnotation
-            let mapItem: MKMapItem
+                }
+            }
         }
     }
+    struct Marker: View {
+        var mapItem: MKMapItem
+        var body: some View {
+            if let url = mapItem.url {
+                Link(destination: url, label: {
+                    Image("pizza")
+                })
+            }
+        }
+    }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+struct Place: Identifiable {
+    let id = UUID()
+    let annotation: MKPointAnnotation
+    let mapItem: MKMapItem
 }
